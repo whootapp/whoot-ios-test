75406b5c090600181d413a81d8b2498559fcfe1d
Carlos Estrada <me@caestrada.com>
2019-12-18 12:21:49 -0800

    Initial Commit

---
+++ whoot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whoot
+//
+//  Created by Carlos Estrada on 12/18/19.
+//  Copyright © 2019 Whoot. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
